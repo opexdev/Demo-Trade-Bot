490a15c3a863885d785f16d1ac65374559b60883
metalicn20 <metalicn20@gmail.com>
2021-09-07 01:08:14 +0430

    Add simple action

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("hello, typescript!");
+import axiosInstance from "axios";
+
+(async function () {
+  const axios = axiosInstance.create({});
+  try {
+    // const res = await axios.get(
+    //   "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+    // );
+    const data = { price: "51844.40000000" };
+    const input = Number.parseFloat(data.price);
+    // 1 buy request with price between binance-binance%101
+    {
+      const price = input + Math.random() * (0.01 * input);
+    }
+    // 10 buy requests with price between binance%90-binance
+    for (let i = 0; i < 10; i++) {
+      const price = input - Math.random() * (0.1 * input);
+    }
+    // 1 sell request with price between binance%99-binance
+    {
+      const price = input - Math.random() * (0.01 * input);
+    }
+    // 10 sell requests with price between binance-binance%110
+    for (let i = 0; i < 10; i++) {
+      const price = input + Math.random() * (0.1 * input);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+})();
