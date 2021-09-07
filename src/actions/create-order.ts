050d318db63bacdcf00d33b73c3c642444745f7e
metalicn20 <metalicn20@gmail.com>
2021-09-07 12:44:05 +0430

    Add delay to requests

--- src/actions/create-order.ts
+++ src/actions/create-order.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import delay from "../utils/delay";
 
 const BASE_URL = "https://api.opex.dev/api/v3";
 
@@ -14,7 +15,7 @@ const createOrder = async (
   const epoch = Math.round(Date.now() / 1000);
   const { symbol, side, price, quantity } = body;
   const url = `${BASE_URL}/order?symbol=${symbol}&side=${side}&price=${price}&quantity=${quantity}&type=LIMIT&timeInForce=GTC&timestamp=${epoch}`;
-  await axios.post(url, null, {
+  return axios.post(url, null, {
     headers: {
       ["Authorization"]: `Bearer ${token}`,
     },
@@ -30,56 +31,64 @@ export const createOrderAction = async function (
   // );
   const data = { price: "51844.40000000" };
   const sourcePrice = Number.parseFloat(data.price);
-  // 1 BUY -- source <= price <= source%101
+  // 1 BUY | source <= price <= source%101
   {
     const price = sourcePrice + Math.random() * (0.01 * sourcePrice);
+    const quantity = Math.round(Math.random() * 1000) / 1000;
     const res = await createOrder(
       {
         symbol,
         side: "BUY",
-        price,
-        quantity: Math.round(Math.random() * 300),
+        price: Math.round(price * 100) / 100,
+        quantity,
       },
       authorizationToken
     );
   }
-  // 10 BUY -- source%90 <= price <= source
+  await delay(9000);
+  // 10 BUY | source%90 <= price <= source
   for (let i = 0; i < 10; i++) {
     const price = sourcePrice - Math.random() * (0.1 * sourcePrice);
+    const quantity = Math.round(Math.random() * 1000) / 1000;
     const res = await createOrder(
       {
         symbol,
         side: "BUY",
-        price,
-        quantity: Math.round(Math.random() * 300),
+        price: Math.round(price * 100) / 100,
+        quantity,
       },
       authorizationToken
     );
+    await delay(9000);
   }
-  // 1 SELL -- source%99 <= price <= source
+  // 1 SELL | source%99 <= price <= source
   {
     const price = sourcePrice - Math.random() * (0.01 * sourcePrice);
+    const quantity = Math.round(Math.random() * 1000) / 1000;
     const res = await createOrder(
       {
         symbol,
         side: "SELL",
-        price,
-        quantity: Math.round(Math.random() * 300),
+        price: Math.round(price * 100) / 100,
+        quantity,
       },
       authorizationToken
     );
   }
-  // 10 SELL -- source <= price <= source%110
+  await delay(9000);
+  // 10 SELL | source <= price <= source%110
   for (let i = 0; i < 10; i++) {
     const price = sourcePrice + Math.random() * (0.1 * sourcePrice);
+    const quantity = Math.round(Math.random() * 1000) / 1000;
     const res = await createOrder(
       {
         symbol,
         side: "SELL",
-        price,
-        quantity: Math.round(Math.random() * 300),
+        price: Math.round(price * 100) / 100,
+        quantity,
       },
       authorizationToken
     );
+    await delay(9000);
   }
 };
