577a5cfceee6270c06548ceecb25624f98c710ae
metalicn20 <metalicn20@gmail.com>
2021-09-07 13:49:15 +0430

    Refactor create order

--- src/actions/create-order.ts
+++ src/actions/create-order.ts
@@ -21,7 +21,7 @@ const createOrder = async (
   });
 };
 
-const getRandomNumber = (min: number, max: number, decimals = 6) => {
+const randomInRange = (min: number, max: number, decimals = 6) => {
   return (
     Math.round((min + Math.random() * (max - min)) * 10 ** decimals) /
     10 ** decimals
@@ -42,8 +42,8 @@ export const createOrderAction = async function (
   const sourcePrice = Number.parseFloat(data.price);
   // 1 BUY | source <= price <= source%101
   {
-    const price = getRandomNumber(sourcePrice, sourcePrice * 1.01, 0);
-    const quantity = getRandomNumber(qtyMin, qtyMax);
+    const price = randomInRange(sourcePrice, sourcePrice * 1.01, 0);
+    const quantity = randomInRange(qtyMin, qtyMax);
     const res = await createOrder(
       {
         symbol,
@@ -57,8 +57,8 @@ export const createOrderAction = async function (
   await delay(delayMs);
   // 10 BUY | source%90 <= price <= source
   for (let i = 0; i < 10; i++) {
-    const price = getRandomNumber(sourcePrice * 0.9, sourcePrice, 0);
-    const quantity = getRandomNumber(qtyMin, qtyMax);
+    const price = randomInRange(sourcePrice * 0.9, sourcePrice, 0);
+    const quantity = randomInRange(qtyMin, qtyMax);
     const res = await createOrder(
       {
         symbol,
@@ -72,8 +72,8 @@ export const createOrderAction = async function (
   }
   // 1 SELL | source%99 <= price <= source
   {
-    const price = getRandomNumber(sourcePrice * 0.99, sourcePrice, 0);
-    const quantity = getRandomNumber(qtyMin, qtyMax);
+    const price = randomInRange(sourcePrice * 0.99, sourcePrice, 0);
+    const quantity = randomInRange(qtyMin, qtyMax);
     const res = await createOrder(
       {
         symbol,
@@ -87,8 +87,8 @@ export const createOrderAction = async function (
   await delay(delayMs);
   // 10 SELL | source <= price <= source%110
   for (let i = 0; i < 10; i++) {
-    const price = getRandomNumber(sourcePrice, sourcePrice * 1.1, 0);
-    const quantity = getRandomNumber(qtyMin, qtyMax);
+    const price = randomInRange(sourcePrice, sourcePrice * 1.1, 0);
+    const quantity = randomInRange(qtyMin, qtyMax);
     const res = await createOrder(
       {
         symbol,
